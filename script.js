7fdbecc0bfe5aae0d3328e7a28f2ca80a5e2a8c6
Hafiztni <114867465+Hafiztni@users.noreply.github.com>
2025-09-18 19:49:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('.container');
+const text = container.querySelector('.text');
+
+container.addEventListener('click', () => {
+  // mulai animasi fingerprint
+  container.classList.add('active');
+
+  // Setelah fingerprint animasi jalan (6 detik), ganti ke SUCCESS
+  setTimeout(() => {
+    text.textContent = "SUCCESS";
+    document.body.classList.add("success-bg");
+
+    // Setelah 3 detik dari munculnya SUCCESS, redirect ke index.html
+    setTimeout(() => {
+      window.location.href = "intro.html";
+    }, 2000);
+
+  }, 5000); // sesuai durasi animasi Container (6s)
+});
